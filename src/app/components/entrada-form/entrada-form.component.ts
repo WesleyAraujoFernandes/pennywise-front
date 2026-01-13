1ccc1864108d99a5226b9e1ddb3069cf8533eb9b
knowledgesolutionstecnology <knowledgesolutionstecnology@gmail.com>
2026-01-13 09:46:26 -0300

    componente de entrada de despesas criado

---
+++ src/app/components/entrada-form/entrada-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entrada-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './entrada-form.component.html',
+  styleUrl: './entrada-form.component.scss'
+})
+export class EntradaFormComponent {
+
+}
